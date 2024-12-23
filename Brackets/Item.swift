dd43b31580cbfe9a151de21b48217b94e6d5ec44
rtrikha <raunakstrikha@gmail.com>
2024-12-23 17:21:28 +0530

    Initial Commit

---
+++ Brackets/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  Brackets
+//
+//  Created by R Trikha on 23/12/24.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
---
+++ BracketsTests/BracketsTests.swift
@@ -0,0 +1,17 @@
+//
+//  BracketsTests.swift
+//  BracketsTests
+//
+//  Created by R Trikha on 23/12/24.
+//
+
+import Testing
+@testable import Brackets
+
+struct BracketsTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
